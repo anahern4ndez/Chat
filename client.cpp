78439620858bf1c914823337074e6ebbeb17c236
Ana Hernandez <her17138@uvg.edu.gt>
2020-04-13 22:16:34 -0600

    1/2 handshake (sep files)
    ya envia info client a server

--- client.cpp
+++ client.cpp
@@ -6,6 +6,9 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include "mensaje.pb.h"
+using namespace std;
+using namespace chat;
 
 void error(const char *msg)
 {
@@ -15,20 +18,22 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
 
     char buffer[256];
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
+    
+    if (argc != 4) {
+       fprintf(stderr, "./client [username] [host] [port]\n");
+       exit(1);
     }
-    portno = atoi(argv[2]);
+    portno = atoi(argv[3]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) 
         error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
+    server = gethostbyname(argv[2]);
     if (server == NULL) {
         fprintf(stderr,"ERROR, no such host\n");
         exit(0);
@@ -41,17 +46,33 @@ int main(int argc, char *argv[])
     serv_addr.sin_port = htons(portno);
     if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = write(sockfd, buffer, strlen(buffer));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd, buffer, 255);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n", buffer);
+
+    cout << "connect" << endl;
+    /*
+        three way handshake 
+    */
+
+    MyInfoSynchronize * clientInfo(new MyInfoSynchronize);
+    clientInfo->set_username(argv[1]);
+    clientInfo->set_ip(argv[2]);
+
+    // Se crea instancia de Mensaje, se setea los valores deseados
+    ClientMessage clientMessage;
+    clientMessage.set_option(1);
+    clientMessage.set_allocated_synchronize(clientInfo);
+
+    // Se serializa el message a string
+    string binary;
+    clientMessage.SerializeToString(&binary);
+    
+    // envio de mensaje de cliente a server 
+    char cstr[binary.size() + 1];
+    strcpy(cstr, binary.c_str());
+
+    // send to socket
+    send(sockfd, cstr, strlen(cstr), 0 );
+
+        // listen for server response
     close(sockfd);
     return 0;
 }

3e24bccd5757927ddcc9a778e0dbc6e66c6c74ff
Ana Hernandez <her17138@uvg.edu.gt>
2020-04-16 19:16:58 -0600

    cambio y adición de estructuras para el server
    se implementó una mutex queue para encolar a los clientes conectados

--- server.cpp
+++ server.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <stdio.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/select.h>
+#include <netdb.h>
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <unistd.h>
@@ -10,27 +12,71 @@
 #include <string.h>
 #include <arpa/inet.h>
 #include <unordered_map>
+#include <fcntl.h>
 #include "mensaje.pb.h"
 using namespace chat;
 
-#define BACKLOG 5
-#define MAX_BUFFER 8192
-#define CLI_OPTION_SYNC 1
-#define CLI_OPTION_USERS 2
-#define CLI_OPTION_STAT 3
-#define CLI_OPTION_BROADCAST 4
-#define CLI_OPTION_DM 5
+#define QUEUE_MAX 10 // establece el numero maximo de conexiones en cola 
+#define MAX_BUFFER 8192 //cantidad maxima de caracteres en un mensaje
 
-struct ChatClient
+//se nombrarán las funciones aquí pero las implementaciones están de último
+void bind_socket(struct sockaddr_in serverAddr, int socketFd, long port);
+void *client_thread(void *params);
+void ErrorToClient(int socketFd, std::string errorMsg);
+
+/*
+    Se usará una queue para monitorear a los clientes. Contiene mutex locks para cuando 
+    se agrega, modifica o elimina a un cliente de a lista, y variables condición para cuando esté llena o vacía. 
+*/
+typedef struct {
+    char *buffer[MAX_BUFFER];
+    int head, tail;
+    int full, empty;
+    pthread_mutex_t *mutex;
+    pthread_cond_t *notFull, *notEmpty;
+} client_queue;
+
+/*
+    En este struct se guardan las queues y threads necesarios para el manejo 
+*/
+typedef struct {
+    int connected_clients[MAX_BUFFER]; //lista donde se guardan los FDs de los sockets de clientes conectados
+    pthread_mutex_t *client_queue_mutex; //lock para poder modificar la lista de clientes conectados
+    client_queue *queue;
+} chat_data;
+
+// opciones de mensaje para el cliente
+enum ClientOpt {
+    SYNC = 1,
+    CONNECTED_USERS = 2,
+    STATUS = 3,
+    BROADCAST_C =4,
+    DM = 5
+};
+// opciones de mensaje para el server
+enum ServerOpt {
+    BROADCAST_S = 1,
+    MESSAGE = 2,
+    ERROR = 3,
+    RESPONSE = 4,
+    C_USERS_RESPONSE = 5,
+    CHANGE_STATUS = 6,
+    BOADCAST_RESPONSE = 7,
+    DM_RESPONSE = 8
+};
+
+ /*
+    Struct que contiene toda la información relevante del cliente. 
+ */
+struct Client
 {
     int socketFd;
     std::string username;
-    char ipAddr[INET_ADDRSTRLEN];
+    char ip_address[INET_ADDRSTRLEN];
     std::string status;
 };
 
-std::unordered_map<std::string, ChatClient *> clients;
-
+std::unordered_map<std::string, Client *> clients;
 
 
 void error(const char *msg)
@@ -39,7 +85,55 @@ void error(const char *msg)
     exit(1);
 }
 
+int main(int argc, char *argv[])
+{
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+    int sockfd, newsockfd;
+    socklen_t clilen;
+    sockaddr_in serv_addr, cli_addr;
+    long port = 9999; // el puerto default es 9999
+    char cli_addr_addr[INET_ADDRSTRLEN];
 
+    if (argc == 2)
+    {
+        port = atoi(argv[1]);
+    }
+
+    /* 
+        La siguiente conexión y binding a un socket se tomó de:
+        https://www.bogotobogo.com/cplusplus/sockets_server_client.php
+    */
+    // create a socket
+    sockfd =  socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+    error("ERROR opening socket");    
+    // clear address structure
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    bind_socket(serv_addr, sockfd, port);
+    // El socket actual queda abierto para nuevas conexiones, hasta que se les haga accept() quedan en cola, 
+    // el número máximo de elementos en cola es QUEUE_MAX.
+    if (listen(sockfd, QUEUE_MAX) == -1)
+    {
+        close(sockfd);
+        error("ERROR on listening.\n");
+    }
+    printf("Listening...  %ld\n", port);
+    // Se quedara esperando nuevas conexiones 
+    while (1)
+    {
+        // el accept accederá a una nueva conexión con un cliente. Crea un nuevo socket para que el anterior 
+        // pueda quedarse escuchando para otras nuevas conexiones. 
+        clilen = sizeof cli_addr;
+        newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+        if (newsockfd < 0)
+            error("ERROR en accept()");
+        pthread_t thread_cli;
+        pthread_create(&thread_cli, NULL, client_thread, (void *)&newsockfd);
+    }
+
+    google::protobuf::ShutdownProtobufLibrary();
+    return 0;
+}
 
 void ErrorToClient(int socketFd, std::string errorMsg)
 {
@@ -55,9 +149,9 @@ void ErrorToClient(int socketFd, std::string errorMsg)
     send(socketFd, buffer, sizeof buffer, 0);
 }
 
-void *worker_thread(void *params)
+void *client_thread(void *params)
 {
-    struct ChatClient thisClient;
+    struct Client thisClient;
     int socketFd = *(int *)params;
     char buffer[MAX_BUFFER];
     std::string msgSerialized;
@@ -71,13 +165,11 @@ void *worker_thread(void *params)
     while (1)
     {
         recv(socketFd, buffer, MAX_BUFFER, 0);
-        
-
         // recepcion y parse de mensaje del cliente
         clientMessage.ParseFromString(buffer);
 
         // Un if para cada opcion del cliente
-        if (clientMessage.option() == CLI_OPTION_SYNC)
+        if (clientMessage.option() == ClientOpt::SYNC)
         {
             if (!clientMessage.has_synchronize())
             {
@@ -92,8 +184,6 @@ void *worker_thread(void *params)
             std::cout << "Option: " << clientMessage.option() << std::endl;
             std::cout << "Username: " << clientMessage.synchronize().username() << std::endl;
             std::cout << "ip: " << clientMessage.synchronize().ip() << std::endl;
-
-
                 // envio de response 
             //response build 
             MyInfoResponse * serverResponse(new MyInfoResponse); 
@@ -118,11 +208,11 @@ void *worker_thread(void *params)
             std::cout << "User ID: " << clientAcknowledge.acknowledge().userid() << std::endl;
 
 
-            strcpy(thisClient.ipAddr, clientMessage.synchronize().ip().c_str());
+            strcpy(thisClient.ip_address, clientMessage.synchronize().ip().c_str());
             thisClient.username = clientMessage.synchronize().username();
             thisClient.socketFd = socketFd;
             clients[thisClient.username] = &thisClient;
-            std::cout << "User connected"<< thisClient.username<< std::endl;
+            std::cout << "User connected: "<< thisClient.username<< std::endl;
         }
 
         std::cout << "--- Users:  " << clients.size() << std::endl;
@@ -137,81 +227,14 @@ void *worker_thread(void *params)
     pthread_exit(0);
 }
 
-int main(int argc, char *argv[])
-{
-    GOOGLE_PROTOBUF_VERIFY_VERSION;
-    int sockfd, newsockfd;
-    socklen_t clilen;
-    sockaddr_in serv_addr, cli_addr;
-    long port = strtol(argv[1], NULL, 10);
-    char cli_addr_addr[INET_ADDRSTRLEN];
-
-    if (argc < 2)
-    {
-        fprintf(stderr, "ERROR, no port provided\n");
-        exit(1);
-    }
-    // create a socket
-    // socket(int domain, int type, int protocol)
-    sockfd =  socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-    error("ERROR opening socket");
-    
-    // clear address structure
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-
-    /* setup the host_addr structure for use in bind call */
-    // server byte order
-    serv_addr.sin_family = AF_INET;  
-
-    // automatically be filled with current host's IP address
-    serv_addr.sin_addr.s_addr = INADDR_ANY;  
-
-    // convert short integer value for port must be converted into network byte order
-    serv_addr.sin_port = htons(port);
-
-    memset(serv_addr.sin_zero, 0, sizeof serv_addr.sin_zero);
-
-    // bind(int fd, struct sockaddr *local_addr, socklen_t addr_length)
-    // bind() passes file descriptor, the address structure, 
-    // and the length of the address structure
-    // This bind() call will bind  the socket to the current IP address on port, portno
-    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-            close(sockfd);
+void bind_socket(struct sockaddr_in serverAddr, int socketFd, long port){
+    serverAddr.sin_family = AF_INET;  
+    serverAddr.sin_addr.s_addr = INADDR_ANY;  
+    serverAddr.sin_port = htons(port);
+    memset(serverAddr.sin_zero, 0, sizeof serverAddr.sin_zero);
+    // This bind() call will bind  the socket to the current IP address on port
+    if (bind(socketFd, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) < 0) {
+            close(socketFd);
             error("ERROR on binding\n");
     }
-
-    // This listen() call tells the socket to listen to the incoming connections.
-    // The listen() function places all incoming connection into a backlog queue
-    // until accept() call accepts the connection.
-    // Here, we set the maximum size for the backlog queue to 5.
-    if (listen(sockfd, BACKLOG) == -1)
-    {
-        close(sockfd);
-        error("ERROR on listening.\n");
-    }
-
-    printf("Listening...  %ld\n", port);
-
-    while (1)
-    {
-        // The accept() call actually accepts an incoming connection
-        clilen = sizeof cli_addr;
-
-        // This accept() function will write the connecting client's address info 
-        // into the the address structure and the size of that structure is clilen.
-        // The accept() returns a new socket file descriptor for the accepted connection.
-        // So, the original socket file descriptor can continue to be used 
-        // for accepting new connections while the new socker file descriptor is used for
-        // communicating with the connected client.
-        newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
-        if (newsockfd < 0)
-            error("ERROR en accept()");
-         
-
-        pthread_t thread_cli;
-        pthread_create(&thread_cli, NULL, worker_thread, (void *)&newsockfd);
-    }
-
-    return 0;
 }
